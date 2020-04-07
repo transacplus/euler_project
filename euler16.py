bf879a32325dbd195e9e089c09df3e2dee2b69d4
SteF <slag007@gmail.com>
2020-04-07 13:17:49 +0200

    n^p

---
+++ euler16.py
@@ -0,0 +1,33 @@
+# Euler 16
+# 2 ^ 1000
+# Multiplication 
+
+def n_puissance_p(n,p):
+""" n^p  implémentation de la multiplication avec gestion de la retenue"""
+    lx=[1]
+    lx2=[]
+    retenue =0
+
+    for j in range(p):
+        for i in lx[::-1] : # parcours inverse de la liste
+            t=i*n + retenue
+            if t < 10:
+                lx2.insert(0,t)
+                retenue = 0
+            else :
+                strvaleur= str(t)
+                lx2.insert(0, int(strvaleur[1]))
+                retenue = int(strvaleur[0])
+
+        if retenue !=0 :
+            lx2.insert(0,retenue)
+        retenue = 0
+        lx=lx2
+        lx2=[]
+    return lx
+
+print ((n_puissance_p(2,1000)))  
+print (sum(n_puissance_p(2,1000)))         
+
+
+    
